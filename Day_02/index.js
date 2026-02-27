cce124b2f1bb39d98238893974354a233db64720
Abhimanyu kumar <abhimanyukumar841424cpr@gmail.com>
2026-02-27 08:35:11 +0530

    CJS_Module

---
+++ Day_02/index.js
@@ -0,0 +1,19 @@
+const {sum,sub} = require("./second")
+// require("./third")
+// require("/fourth")
+// require("./fifth")
+
+
+sum(3,4);
+sub(7,8);
+console.log("Hello I am first");
+
+// (function (){
+//     console.log("Hello ji I am second");
+
+//     function sum(a,b){
+//     console.log(a+b);
+//     }
+
+//    sum(4,3);
+// })();
---
+++ Day_02/second.js
@@ -0,0 +1,17 @@
+console.log("Hello ji I am second");
+
+function sum(a,b){
+    console.log(a+b);
+}
+
+function sub(a,b){
+    console.log(a-b);
+}
+
+
+console.log(module.exports);
+
+// module.exports = {sum,sub};
+module.exports.sum = sum;
+module.exports.sub = sub;
+
