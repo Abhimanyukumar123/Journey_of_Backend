c7e21a6a9dd6696d75a40ee11e901b5d71652372
Abhimanyu kumar <abhimanyukumar841424cpr@gmail.com>
2026-03-12 19:01:57 +0530

    Server

---
+++ Day_04/index.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const server = http.createServer((req,res)=>{
+    if(req.url==="/"){
+        res.end("Hello Coder Army");
+    }
+    else if(req.url==="/contact"){
+        res.end("This is our Contact Page")
+    }
+    else if(req.url==="/about"){
+        res.end("This is our About Page")
+    }
+    else
+    {
+        res.end("Error: Page Not Found");
+    }
+});
+server.listen(4000, ()=>{
+    console.log("I am Listening at port number 4000");
+})
