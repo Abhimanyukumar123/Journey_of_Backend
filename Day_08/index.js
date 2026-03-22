4e67ab7839594510bea4e73d90548102f901724e
Abhimanyu kumar <abhimanyukumar841424cpr@gmail.com>
2026-03-22 13:47:51 +0530

    Fix: updated Day 08 and removed Day 11 package.json

--- Day_08/index.js
+++ Day_08/index.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const app = express();
-
-
 const BookStore = [
     {id:1,name:"Harry Potter", author:"DevFlux"},
     {id:2, name:"Friends", author: "Vikas"},
@@ -10,7 +8,6 @@ const BookStore = [
     {id:5, name:"Prem Kahani", author:"Rohan"},
     {id:6, name:"Heloo", author: "Vikas"},
 ]
-
 app.use(express.json());
 
 // localhost:3000/book/3
@@ -21,7 +18,6 @@ app.get("/book", (req,res)=>{
     const Book = BookStore.filter(info => info.author === req.query.author);
     res.send(Book);
 })
-
 app.get("/book/:id", (req,res)=>{
 
     const id = parseInt(req.params.id);
@@ -29,58 +25,35 @@ app.get("/book/:id", (req,res)=>{
     const Book =  BookStore.find(info=> info.id===id);
     res.send(Book); 
 })
-
 app.post("/book", (req,res)=>{
     console.log(req.body);
     BookStore.push(req.body);
     res.send("Data Saved Successfully");
 })
-
 app.patch("/book", (req,res)=>{
     console.log(req.body);
-
    const Book = BookStore.find(info => info.id === req.body.id);
    
    if(req.body.author)
    Book.author = req.body.author;
-
    if(req.body.name)
    Book.name = req.body.name;
-
    res.send("Patch updated")
 })
-
-
 app.put("/book", (req,res)=>{
-
     const Book = BookStore.find(info => info.id === req.body.id);
-    
     Book.author = req.body.author;
     Book.name = req.body.name;
 
     res.send("Changes Updated Succesfully");
 })
-
 app.delete("/book/:id", (req,res)=>{
-
     const id = parseInt(req.params.id);
-    
- 
     const index = BookStore.findIndex(info => info.id === id);
-    
     console.log(index);
     BookStore.splice(index,1);
     res.send("Successfully Deleted");
-
 })
-
-
-
-
-
-
-
-
 app.listen(3000, ()=>{
     console.log("Listening at port 3000");
 })
